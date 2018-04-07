32fb292e2cfb7577b28097efec27dccc0a14204c
Stephane Derrien <stephane.derrien91@gmail.com>
2018-04-07 17:56:59 +0200

    :D

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd Back
 npm install
-ALWAYSDATA_HTTPD_PORT=8100 ALWAYSDATA_HTTPD_IP=127.2.83.196 MYSQL_SERVER=mysql-potiron.alwaysdata.net MYSQL_USER=potiron_admin MYSQL_PASSWORD=canard MYSQL_DATABASE=potiron_db npm start
+#ALWAYSDATA_HTTPD_PORT=8100 ALWAYSDATA_HTTPD_IP=127.2.83.196 MYSQL_SERVER=mysql-potiron.alwaysdata.net MYSQL_USER=potiron_admin MYSQL_PASSWORD=canard MYSQL_DATABASE=potiron_db npm start
