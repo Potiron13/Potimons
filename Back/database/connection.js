e0d503a78835b20d8b2b9adfc739d359453519d3
Stéphane Derrien <stephane.derrien91@gmail.com>
2021-12-16 18:16:32 +0000

    connection.js edited online with Bitbucket

--- Back/database/connection.js
+++ Back/database/connection.js
@@ -3,10 +3,10 @@ let connection = null;
 
 function init() {
     return mysql.createConnection({
-        host: process.env.MYSQL_SERVER || 'localhost',
-        user: process.env.MYSQL_USER || 'root',
-        password: process.env.MYSQL_PASSWORD || 'password',
-        database: process.env.MYSQL_DATABASE || 'potimondb'
+        host: process.env.MYSQL_SERVER || 'mysql-potiron.alwaysdata.net',
+        user: process.env.MYSQL_USER || 'potiron_admin',
+        password: process.env.MYSQL_PASSWORD || 'canardmouton',
+        database: process.env.MYSQL_DATABASE || 'potiron_db'
     }).then(function(conn){    
         connection = conn;
     });
